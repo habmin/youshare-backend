c2a986ee54af267f1bc286d7761d8af917fa9480
Henry Baum <henryandrewbaum@gmail.com>
2020-12-11 02:08:18 -0500

    hmm

--- app.py
+++ app.py
@@ -22,8 +22,9 @@ app.config['SECRET_KEY'] = SECRET;
 app.config['CORS_HEADERS'] = 'Content-Type';
 if 'ON_HEROKU' in os.environ:
     print('extra heroku configs activated');
-    app.config.update(SESSION_COOKIE_SECURE=True, SESSION_COOKIE_SAMESITE='None');
-    
+    app.config['SESSION_COOKIE_SECURE'] = True;
+    app.config['SESSION_COOKIE_SAMESITE'] = 'None';
+
 socketio = SocketIO(app, cors_allowed_origins="*");
 
 app.register_blueprint(session, url_prefix="/api/sessions");
