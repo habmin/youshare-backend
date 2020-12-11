4893601f2fce75536fcf9aba3807c2d81512a064
Henry Baum <henryandrewbaum@gmail.com>
2020-12-11 02:38:13 -0500

    Trying all origins

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ import os;
 PORT = os.getenv("PORT");
 SECRET = os.getenv("SECRET");
 
-CORS(session, origins=['http://localhost:3000', 'https://youshare-frontend.herokuapp.com'], supports_credentials=True);
+CORS(session, origins="*");
 
 app = Flask(__name__);
 app.config['SECRET_KEY'] = SECRET;
@@ -30,7 +30,7 @@ socketio = SocketIO(app, cors_allowed_origins="*");
 
 app.register_blueprint(session, url_prefix="/api/sessions");
 
-CORS(app, origins=['http://localhost:3000', 'https://youshare-frontend.herokuapp.com'], supports_credentials=True);
+CORS(app, origins="*");
 
 
 @app.before_request
